48559730fd6cef91c3a56ac42d7a188f46652d4d
Zablon Samba <zablonsamba@gmail.com>
2023-10-23 11:38:42 -0700

    PYC

---
+++ 0x05-python-exceptions/python.h
@@ -0,0 +1,11 @@
+#ifndef PYTHON_H_
+#define PYTHON_H_
+
+#include <stdio.h>
+#include <string.h>
+
+void print_python_float(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_list(PyObject *p);
+
+#endif 
