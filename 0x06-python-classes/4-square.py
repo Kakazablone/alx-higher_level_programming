cf392e1d05253cbb339231893f003737b7be5e5b
Zablon Samba <zablonsamba@gmail.com>
2023-10-24 05:20:41 -0700

    access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module defining a square class"""
+
+
+class Square:
+    """A class that represents a square"""
+
+    def __init__(self, size=0):
+        """Raise errors on validation fail"""
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """asserts validation"""
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Calculates the area of a square and returns value"""
+        return (self.__size ** 2)
