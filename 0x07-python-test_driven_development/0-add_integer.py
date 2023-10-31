cf72f536689d735cf63ae42526a1cd09ff7be9a3
Zablon Samba <zablonsamba@gmail.com>
2023-10-31 09:23:07 -0700

    Implement add integer test cases

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""My addition module
+
+add_integer: adds two integers together
+
+"""
+
+
+def add_integer(a, b=98):
+    """Returns a + b
+    Args: a and b (int): the numbers to add
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
