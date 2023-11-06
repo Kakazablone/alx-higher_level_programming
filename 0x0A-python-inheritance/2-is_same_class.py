9cba43a501a233d94baa86171d412458436fff35
Zablon Samba <zablonsamba@gmail.com>
2023-11-06 02:26:44 -0800

    Perform a boolean on type

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-"""Defines a function that checks if an object
-is exactly an instance of a particular class"""
+""" My Module one testing objects
+"""
 
 
 def is_same_class(obj, a_class):
-    return type(obj) == a_class
+    """ Function that tests whether an object is part of a class
+
+    Args:
+        obj (object): the object to test whether it is part of a class
+        a_class (class): tests if obj is part of it
+
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
