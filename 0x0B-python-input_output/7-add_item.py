137da5b5da6339439f453535f89e381000759e3c
Zablon Samba <zablonsamba@gmail.com>
2023-11-07 21:05:20 -0800

    Implement Log parsing a different way

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,27 +1,45 @@
 #!/usr/bin/python3
-"""Script that reads stdin line by line and computes metrics"""
+"""Reads from standard input and computes metrics.
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
+"""
+
 import sys
 
-status_codes = {}
+"""Initialize variables to store metrics"""
 total_size = 0
-lines_read = 0
+s_codezcount = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
+
+def print_statistics():
+    print(f"File size: {total_size}")
+
+    """Print number of lines by status code in ascending order"""
+    for status_code in sorted(s_codezcount):
+        count = s_codezcount[status_code]
+        if count > 0:
+            print(f"{status_code}: {count}")
 
 try:
-    with sys.stdin as file:
-        """Reading and parsing lines"""
-        for line in file:
-            line = line.split(" ")
-            if line[-2] in status_codes:
-                status_codes[line[-2]] += 1
-            else:
-                status_codes[line[-2]] = 1
-            total_size += int(line[-1])
-            lines_read += 1
-            if lines_read % 10 == 0:
-                print("File size:", total_size)
-                for code in sorted(status_codes):
-                    print("{}: {}".format(code, status_codes[code))
-finally:
-    print("File size:", total_size)
-    for code in sorted(status_codes):
-        print("{}: {}".format(code, status_codes[code))
+    for line in sys.stdin:
+        """Split the input line and extract the status code and file size"""
+        line_toks = line.split()
+        if len(line_toks) >= 8:
+            status_code = int(line_toks[-2])
+            file_size = int(line_toks[-1])
+
+	    """Update total file size and status code counts"""
+            total_size += file_size
+            if status_code in s_codezcount:
+                s_codezcpont[status_code] += 1
+
+            """Increment line count"""
+            line_count += 1
+
+            """Print statistics every 10 lines"""
+            if line_count % 10 == 0:
+                print_statistics()
+except KeyboardInterrupt:
+    print_statistics()
