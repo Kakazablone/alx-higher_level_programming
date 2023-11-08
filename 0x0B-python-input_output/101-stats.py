4b3700b1d5d0081862b42226fd066bfcc9b4118c
Zablon Samba <zablonsamba@gmail.com>
2023-11-08 01:41:14 -0800

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,13 +5,16 @@ prints the following statistics:
     - Total file size up to that point.
     - Count of read status codes up to that point.
 """
+
 import sys
 
+
 def print_metrics(total_file_size, s_code_count):
-    print("File size: ", total_file_size)
+    print("File size:", total_file_size)
     for code in sorted(s_code_count.keys()):
         if s_code_count[code] > 0:
-            print("{}: {}".format(code, s_code_count[code))
+            print("{}: {}".format(code, s_code_count[code]))
+
 
 s_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 total_file_size = 0
@@ -19,7 +22,7 @@ total_file_size = 0
 try:
     line_count = 0
     for line in sys.stdin:
-        line_toks = line.split(" ")
+        line_toks = line.split()
         if len(line_toks) >= 8:
             status_code = int(line_toks[-2])
             file_size = int(line_toks[-1])
