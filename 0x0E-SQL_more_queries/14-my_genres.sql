046e313bdb28afa5a783f9b99bf5ebb37d9aade2
Zablon Samba <zablonsamba@gmail.com>
2023-12-14 08:51:41 -0800

    uses the hbtn_0d_tvshows database to lists all genres of the show Dexter

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+SELECT tv_genres.name AS name
+FROM tv_show_genres
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE title = "Dexter"
+ORDER BY name;
